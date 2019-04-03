bc17923802f49674883379075a3f303d0a086e1f
heather <unphurlng@gmail.com>
2019-04-03 09:54:56 -0700

    findById completed

--- test/Memory-db.test.js
+++ test/Memory-db.test.js
@@ -7,14 +7,39 @@ describe('MemoryDatabase', () => {
   });
 
   it('creates empty store object', () => {
+    expect(memory.store).toEqual({});
+  });
+
+  it('creates uuid object for dog', () => {
+    // arrange : object/user input
     const dog = {
+      name: 'spot',
+      sex: 'female',
+    };
+
+    // act : created object = create a uuid + add
+    const expected = {
       name: 'spot',
       sex: 'female',
       _id: expect.any(String)
     };
-    const userObject = memory.create(dog);
-    console.log('0000000000000', userObject);
-    expect(memory.store[id]).toEqual(dog);
+    
+    // assert
+    expect(memory.create(dog)).toEqual(expected);
+  });
+
+  it('find object by the id', () => {
+    //arrange
+    const userDog = {
+      name: 'spot',
+      sex: 'female'
+    };
+    
+    const createUserDog = memory.create(userDog);
+    const id = createUserDog._id;
+  
+    //assert
+    expect(memory.findById(id)).toEqual(createUserDog);
   });
 });
 
