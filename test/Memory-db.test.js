e2ec3219f0d608f5262b2f7d91cefce9c839774a
heather <unphurlng@gmail.com>
2019-04-03 13:04:48 -0700

    removed comments

--- test/Memory-db.test.js
+++ test/Memory-db.test.js
@@ -11,36 +11,29 @@ describe('MemoryDatabase', () => {
   });
 
   it('creates uuid object for dog', () => {
-    // arrange : object/user input
     const dog = {
       name: 'spot',
       sex: 'female',
     };
 
-    // act : created object = create a uuid + add
     const expected = {
       name: 'spot',
       sex: 'female',
       _id: expect.any(String)
     };
     
-    // assert
     expect(memory.create(dog)).toEqual(expected);
   });
 
   it('find object by the id', () => {
-    //arrange
     const userDog = {
       name: 'spot',
       sex: 'female'
     };
-    
+
     const createUserDog = memory.create(userDog);
     const id = createUserDog._id;
   
-    //assert
     expect(memory.findById(id)).toEqual(createUserDog);
   });
 });
-
-
